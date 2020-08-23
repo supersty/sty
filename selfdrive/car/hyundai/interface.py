008966767439a17402a7daabc9630dfb228a77be
dragonpilot <52149710+dragonpilot@users.noreply.github.com>
2020-08-23 15:13:32 +1000

    bug fixes

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -359,7 +359,10 @@ class CarInterface(CarInterfaceBase):
 
   def apply(self, c):
     can_sends = self.CC.update(c.enabled, self.CS, self.frame, c.actuators,
+                               c.cruiseControl.cancel, c.hudControl.visualAlert, c.hudControl.leftLaneVisible,
                                c.hudControl.rightLaneVisible, c.hudControl.leftLaneDepart, c.hudControl.rightLaneDepart,
-                               c.hudControl.setSpeed, c.hudControl.leadVisible, self.dragonconf)
+                               c.hudControl.setSpeed, c.hudControl.leadVisible, self.dragonoilot)
     self.frame += 1
+
+
     return can_sends
